73c91aeb8de79ef9eb596d71335e54b27b6a7b4b
SW <hsuapang81@gmail.com>
2020-03-07 20:12:45 +0800

    improve version

--- guess_num.py
+++ guess_num.py
@@ -1,17 +1,16 @@
 import random
 r = random.randint(1,100)
-b = 0
+count = 0
 while True:
-	x = input("請輸入一個1~100的隨機整數: ")
-	x = int(x)
-	if x == r :
+	count += 1 # count = count + 1
+	num = input("請輸入一個1~100的隨機整數: ")
+	num = int(num)
+	if num == r :
 		print('恭喜猜對了!')
-		b = b + 1
-		print('總共猜了', b,'次')
+		print('總共猜了', count,'次')
 		break
-	elif x > r :
-		print('錯了,比', x,'再小一點')
-		b = b + 1
-	elif x < r :
-		print('不對喔，要比', x, '再大一點')
-		b = b + 1
+	elif num > r:
+		print('錯了,比', num,'再小一點')
+	elif num < r:
+		print('不對喔，要比', num,'再大一點')
+	print('這是你猜的第', count,'次')
